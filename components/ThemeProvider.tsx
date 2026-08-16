62905d1a7c0e9b380a512db5235b47bd30ad0899
Jaydenlaoyx <ylao0002@student.monash.edu>
2026-08-16 20:33:48 +1000

    Fix theme hydration warning

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -14,6 +14,7 @@ export default function ThemeProvider({
       attribute="class"
       defaultTheme="dark"
       enableSystem={false}
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
